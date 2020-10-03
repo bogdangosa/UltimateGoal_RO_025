95d03cfe1e53d94059734b955b01b3c874465b63
Mia781 <56364776+Mia781@users.noreply.github.com>
2020-10-03 19:08:44 +0300

    Mia FIRST HardwareMap + Mia FIRST TeleOP

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mia_FIRST_TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Mia_FIRST_TeleOp.java
@@ -27,12 +27,10 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package org.firstinspires.ftc.robotcontroller.external.samples;
+package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
